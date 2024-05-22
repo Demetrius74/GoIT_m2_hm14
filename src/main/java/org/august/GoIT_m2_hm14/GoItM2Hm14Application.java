5d54310786a5ea19d0df5a059ae6ae86cad78f16
rpgthmp <dimatjasko@gmail.com>
2024-05-23 02:31:14 +0300

    Initial commit???

---
+++ src/main/java/org/august/GoIT_m2_hm14/GoItM2Hm14Application.java
@@ -0,0 +1,13 @@
+package org.august.GoIT_m2_hm14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoItM2Hm14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoItM2Hm14Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/august/GoIT_m2_hm14/GoItM2Hm14ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.august.GoIT_m2_hm14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoItM2Hm14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
